46153f9266da784c525fcb8b6461387e83e714cb
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-31 13:34:02 +0200

    ajout de css

--- index.php
+++ index.php
@@ -4,16 +4,22 @@ session_start(); //démarage de session
 $_SESSION['lastname'] = 'Cousin Ruby';
 $_SESSION['firstname'] ='Cédric';
 $_SESSION['age'] = '34';
- ?>
+?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8" />
-    <title>Exercice 2</title>
-  </head>
-  <body>
-    <p>
-      <a href="user.php">Cliquez ici!!</a>
-    </p>
-  </body>
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice 2</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" hrefœ="assetes/css/style.css">
+</head>
+<body>
+  <a href="user.php"type="button" class="btn btn-primary btn-lg btn-block btn-info">Cliquez ici!!</a>
+
+
+  <script src="assets/js/jquery-3.3.1.min.js" charset="utf-8"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" charset="utf-8"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" charset="utf-8"></script>
+  <script src="assets/js/script.js" charset="utf-8"></script>
+</body>
 </html>
--- user.php
+++ user.php
@@ -1,16 +1,32 @@
 <?php
 session_start();
- ?>
- <!DOCTYPE html>
- <html lang="fr" dir="ltr">
-   <head>
-     <meta charset="utf-8" />
-     <title>User page</title>
-   </head>
-   <body>
-     <p>
-       Rebonjour <?= print_r($_SESSION['lastname'] . ' ' . $_SESSION['firstname'] .'!'); ?><br />
-       Tu as toujours <?= print_r($_SESSION['age'] . ' ans?'); ?>
-     </p>
-   </body>
- </html>
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>User page</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+  <div class="info">
+  <div class="card border-primary mb-3" style="max-width: 18rem;">
+    <div class="card-header">Salutation</div>
+    <div class="card-body text-primary">
+      <h5 class="card-title">HELLO</h5>
+      <p class="card-text">
+        Re-bonjour <?= print_r($_SESSION['lastname'] . ' ' . $_SESSION['firstname'] .'!'); ?><br />
+        Tu as toujours <?= print_r($_SESSION['age'] . ' ans?'); ?>
+      </p>
+    </div>
+  </div>
+  </div
+
+
+
+  <script src="assets/js/jquery-3.3.1.min.js" charset="utf-8"></script>
+  <script src="assets/js/script.js" charset="utf-8"></script>
+</body>
+</html>
